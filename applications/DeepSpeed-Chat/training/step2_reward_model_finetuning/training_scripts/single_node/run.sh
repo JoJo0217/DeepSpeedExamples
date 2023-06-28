a6fddca599bfd03a6d79e856dbd4a2a709798ec7
pyross <100517226+JoJo0217@users.noreply.github.com>
2023-06-28 09:16:16 +0900

    Update and rename run_350m.sh to run.sh

--- applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/single_node/run.sh
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/single_node/run.sh
@@ -9,21 +9,21 @@ if [ "$OUTPUT" == "" ]; then
     OUTPUT=./output
 fi
 if [ "$ZERO_STAGE" == "" ]; then
-    ZERO_STAGE=0
+    ZERO_STAGE=2
 fi
 mkdir -p $OUTPUT
 
 deepspeed main.py \
-   --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
+   --data_path local/jsonfile \
    --data_split 2,4,4 \
-   --model_name_or_path facebook/opt-350m \
+   --model_name_or_path EleutherAI/polyglot-ko-12.8b \
    --num_padding_at_beginning 1 \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
    --max_seq_len 512 \
    --learning_rate 5e-5 \
    --weight_decay 0.1 \
-   --num_train_epochs 1 \
+   --num_train_epochs 2 \
    --disable_dropout \
    --gradient_accumulation_steps 1 \
    --lr_scheduler_type cosine \
