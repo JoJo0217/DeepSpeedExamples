af9aa34d2ce7b98f81f0af32c5cd4207e5ec62ab
pyross <100517226+JoJo0217@users.noreply.github.com>
2023-06-28 09:51:08 +0900

    Update run.sh

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run.sh
@@ -17,8 +17,8 @@ deepspeed main.py \
    --data_path local/jsonfile \
    --data_split 2,4,4 \
    --model_name_or_path EleutherAI/polyglot-ko-1.3b \
-   --per_device_train_batch_size 8 \
-   --per_device_eval_batch_size 8 \
+   --per_device_train_batch_size 1 \
+   --per_device_eval_batch_size 1 \
    --max_seq_len 512 \
    --learning_rate 9.65e-6 \
    --weight_decay 0. \
