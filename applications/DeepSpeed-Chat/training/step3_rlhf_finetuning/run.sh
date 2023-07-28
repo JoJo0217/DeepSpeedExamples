21ce7a4953c29d655bdc2db88e876daaf5057070
pyross <100517226+JoJo0217@users.noreply.github.com>
2023-07-28 12:42:10 +0900

    Update run.sh

--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/run.sh
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/run.sh
@@ -12,17 +12,17 @@ if [ "$OUTPUT" == "" ]; then
     OUTPUT=./output
 fi
 if [ "$ACTOR_ZERO_STAGE" == "" ]; then
-    ACTOR_ZERO_STAGE=2
+    ACTOR_ZERO_STAGE=3
 fi
 if [ "$CRITIC_ZERO_STAGE" == "" ]; then
-    CRITIC_ZERO_STAGE=2
+    CRITIC_ZERO_STAGE=3
 fi
 mkdir -p $OUTPUT
 rm -rf /tmp/data_files/
-Num_Padding_at_Beginning=1 # this is model related
+Num_Padding_at_Beginning=0 # this is model related
 
-Actor_Lr=9.65e-6
-Critic_Lr=5e-6
+Actor_Lr=5e-6
+Critic_Lr=9e-6
 
 deepspeed --master_port 12346 main.py \
    --data_path local/jsonfile \
@@ -33,16 +33,16 @@ deepspeed --master_port 12346 main.py \
    --per_device_train_batch_size 2 \
    --per_device_mini_train_batch_size 2 \
    --generation_batch_numbers 1 \
-   --ppo_epochs 2 \
-   --max_answer_seq_len 256 \
-   --max_prompt_seq_len 256 \
+   --ppo_epochs 1 \
+   --max_answer_seq_len 512 \
+   --max_prompt_seq_len 512 \
    --actor_learning_rate ${Actor_Lr} \
    --critic_learning_rate ${Critic_Lr} \
-   --num_train_epochs 2 \
+   --num_train_epochs 1 \
    --lr_scheduler_type cosine \
-   --gradient_accumulation_steps 1 \
+   --gradient_accumulation_steps 32 \
    --disable_actor_dropout \
-   --num_warmup_steps 100 \
+   --num_warmup_steps 10 \
    --deepspeed --seed 1234 \
    --enable_hybrid_engine \
    --actor_lora_dim 128 \
@@ -50,7 +50,6 @@ deepspeed --master_port 12346 main.py \
    --critic_lora_dim 128 \
    --critic_lora_module_name query_key_value \
    --only_optimize_lora \
-   --offload \
    --actor_zero_stage $ACTOR_ZERO_STAGE \
    --critic_zero_stage $CRITIC_ZERO_STAGE \
    --output_dir $OUTPUT 
