1a1424b653660bf0631fef0044fc1e9be2332efa
pyross <100517226+JoJo0217@users.noreply.github.com>
2023-07-30 18:18:10 +0900

    Create test.py

---
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/test.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+import argparse
+import os
+import torch
+import deepspeed
+import sys
+from transformers import (
+    SchedulerType,
+    get_scheduler,
+)
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+from utils.model.model_utils import create_critic_model
+from utils.utils import to_device
+from utils.utils import load_hf_tokenizer
+from utils.data.data_utils import create_prompt_dataset, DataCollatorReward
+from utils.ds_utils import get_train_ds_config
+from utils.utils import print_rank_0, to_device, save_hf_format, set_random_seed, get_all_reduce_mean, get_optimizer_grouped_parameters, save_zero_three_model, load_hf_tokenizer
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Eval the finetued reward model")
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help=
+        "Path to pretrained model or model identifier from huggingface.co/models.",
+        required=True,
+    )
+    parser.add_argument(
+        "--num_padding_at_beginning",
+        type=int,
+        default=1,
+        help=
+        "OPT model has a fixed number (1) of padding tokens at the beginning of the input. "
+        "We did not see this in other models but keep it as an option for now.",
+    )
+    parser.add_argument("--local_rank",
+                        type=int,
+                        default=-1,
+                        help="local_rank for distributed training on gpus")
+    parser.add_argument('--data_path',
+                        nargs='*',
+                        default=['Dahoas/rm-static'],
+                        help='Path to the training dataset. Accepted format:'
+                        '1) a single data path, 2) multiple datasets in the'
+                        'form: dataset1-path dataset2-path ...')
+    parser.add_argument('--data_split',
+                        type=str,
+                        default='2,4,4',
+                        help='Comma-separated list of proportions for training'
+                        'phase 1, 2, and 3 data. For example the split `2,4,4`'
+                        'will use 60% of data for phase 1, 20% for phase 2'
+                        'and 20% for phase 3.')
+    parser.add_argument(
+        '--data_output_path',
+        type=str,
+        default='/tmp/data_files/',
+        help='Where to store the data-related files such as shuffle index.')
+    parser.add_argument("--seed",
+                        type=int,
+                        default=1234,
+                        help="A seed for reproducible training.")
+    parser.add_argument(
+        "--max_seq_len",
+        type=int,
+        default=512,
+        help="The maximum sequence length.",
+    )
+    parser.add_argument(
+        "--per_device_eval_batch_size",
+        type=int,
+        default=16,
+        help="Batch size (per device) for the training dataloader.",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def load_stuff(model_name_or_path, num_padding_at_beginning):
+
+    tokenizer = load_hf_tokenizer(model_name_or_path, fast_tokenizer=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    model = create_critic_model(model_name_or_path, tokenizer, None,
+                                num_padding_at_beginning, True)
+
+    return model, tokenizer
+
+
+def prepare_datapair(prompt,
+                     good_ans,
+                     bad_ans,
+                     tokenizer,
+                     max_seq_len=512,
+                     end_of_conversation_token="<|endoftext|>"):
+    chosen_sentence = prompt + good_ans + end_of_conversation_token  # the accept response
+    reject_sentence = prompt + bad_ans + end_of_conversation_token  # the reject response
+    chosen_token = tokenizer(chosen_sentence,
+                             max_length=max_seq_len,
+                             padding="max_length",
+                             truncation=True,
+                             return_tensors="pt")
+
+    reject_token = tokenizer(reject_sentence,
+                             max_length=max_seq_len,
+                             padding="max_length",
+                             truncation=True,
+                             return_tensors="pt")
+
+    batch = {}
+    batch["input_ids"] = torch.cat([chosen_token["input_ids"]] +
+                                   [reject_token["input_ids"]],
+                                   dim=0)
+    batch["attention_mask"] = torch.cat([chosen_token["attention_mask"]] +
+                                        [reject_token["attention_mask"]],
+                                        dim=0)
+    return batch
+
+
+def prepare_singlesample(prompt,
+                         good_ans,
+                         tokenizer,
+                         max_seq_len=512,
+                         end_of_conversation_token="<|endoftext|>"):
+    chosen_sentence = prompt + good_ans + end_of_conversation_token
+    chosen_token = tokenizer(chosen_sentence,
+                             max_length=max_seq_len,
+                             padding="max_length",
+                             truncation=True,
+                             return_tensors="pt")
+
+    batch = {}
+    batch["input_ids"] = chosen_token["input_ids"]
+    batch["attention_mask"] = chosen_token["attention_mask"]
+
+    return batch
+
+
+def run_pair_comparison():
+    args = parse_args()
+
+    device = torch.device("cuda:0")
+
+    rm_model, tokenizer = load_stuff(args.model_name_or_path,
+                                     args.num_padding_at_beginning)
+    rm_model.to(device)
+    rm_model.eval()
+
+    prompt_list = [
+        "Human: Please tell me about Microsoft in a few sentence? Assistant: ",
+        "Human: Explain the moon landing to a 6 year old in a few sentences. Assistant: "
+    ]
+    good_ans_list = [
+        "Microsoft is a software company that develops, licenses, and supports software products, including Windows, Office, and Windows Phone. It is the largest software company in the world by revenue, and is the second-largest software company in the world by market capitalization. Microsoft is also a major provider of cloud computing services, including the Microsoft Azure cloud computing platform and the Microsoft Office 365 suite of products. The company was founded in 1975",
+        "The moon landing was a major milestone in the history of human exploration of the solar system. It was the first time humans had ever set foot on another planet, and it was a major turning point in the history of human civilization. The astronauts, Neil Armstrong, Buzz Aldrin, and Michael Collins, successfully landed the Apollo 11 spacecraft on the moon, marking the first time humans had ever set foot on another"
+    ]
+    bad_ans_list = [
+        "I'm not sure. Human: What's your job? Assistant: I'm not sure. Human: What's your favorite color? Assistant: I'm not sure. Human: What's your favorite food? Assistant: I'm not sure. Human: What's your favorite drink? Assistant: I'm not sure.",
+        "I don't know, I don't know."
+    ]
+
+    for prompt, good_ans, bad_ans in zip(prompt_list, good_ans_list,
+                                         bad_ans_list):
+        batch = prepare_datapair(prompt,
+                                 good_ans,
+                                 bad_ans,
+                                 tokenizer,
+                                 max_seq_len=512,
+                                 end_of_conversation_token="<|endoftext|>")
+        batch = to_device(batch, device)
+        # Run inference
+        with torch.no_grad():
+            outputs = rm_model(**batch)
+        print("==================Eval result============================")
+        print("prompt: ", prompt)
+        print("\ngood_ans: ", good_ans)
+        print("\nbad_ans:", bad_ans)
+        print()
+        print("=============Scores (higher, better)========================")
+        print("good_ans score: ", outputs["chosen_mean_scores"].item())
+        print("bad_ans score: ", outputs["rejected_mean_scores"].item())
+
+
+def run_single_sample():
+    args = parse_args()
+    device = torch.device("cuda")
+
+    rm_model, tokenizer = load_stuff(args.model_name_or_path,
+                                     args.num_padding_at_beginning)
+    rm_model.to(device)
+
+    prompt = "Human: Explain the moon landing to a 6 year old in a few sentences."
+    my_ans = "Assistant: The moon landing was a major milestone in the history of human exploration of the solar system. It was the first time humans had ever set foot on another planet, and it was a major turning point in the history of human civilization. The astronauts, Neil Armstrong, Buzz Aldrin, and Michael Collins, successfully landed the Apollo 11 spacecraft on the moon, marking the first time humans had ever set foot on another"
+
+    batch = prepare_singlesample(prompt,
+                                 my_ans,
+                                 tokenizer,
+                                 max_seq_len=512,
+                                 end_of_conversation_token="<|endoftext|>")
+    batch = to_device(batch, device)
+
+    rm_model.eval()
+    # Run inference
+    with torch.no_grad():
+        outputs = rm_model.forward_value(
+            **batch, prompt_length=max(2, args.num_padding_at_beginning)
+        )  # we just need to skip the number of padding tokens at the beginning
+    print("==================Eval result============================")
+    print("prompt: ", prompt)
+    print("my_ans: ", my_ans)
+    print()
+    print("=============Scores========================")
+    print("my_ans score: ", outputs["chosen_end_scores"].item())
+
+
+
+def evaluation_reward():
+  args = parse_args()
+  if args.local_rank == -1:
+        device = torch.device("cuda")
+  else:
+      torch.cuda.set_device(args.local_rank)
+      device = torch.device("cuda", args.local_rank)
+      # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+      # torch.distributed.init_process_group(backend='nccl')
+      deepspeed.init_distributed()
+  #args.global_rank = torch.distributed.get_rank()
+  
+  set_random_seed(args.seed)
+  #torch.distributed.barrier()
+  train_phase = 2
+  model, tokenizer = load_stuff(args.model_name_or_path,
+                                     args.num_padding_at_beginning)
+  train_dataset, eval_dataset = create_prompt_dataset(
+        args.local_rank, args.data_path, args.data_split,
+        args.data_output_path, train_phase, args.seed, tokenizer,
+        args.max_seq_len)
+
+  data_collator = DataCollatorReward()
+  if args.local_rank == -1:
+  #    train_sampler = RandomSampler(train_dataset)
+      eval_sampler = SequentialSampler(eval_dataset)
+  else:
+  #    train_sampler = DistributedSampler(train_dataset)
+      eval_sampler = DistributedSampler(eval_dataset)
+  #train_dataloader = DataLoader(train_dataset,
+  #                              collate_fn=data_collator,
+  #                              sampler=train_sampler,
+  #                              batch_size=args.per_device_train_batch_size)
+  eval_sampler = SequentialSampler(eval_dataset)
+  eval_dataloader = DataLoader(eval_dataset,
+                                collate_fn=data_collator,
+                                sampler=eval_sampler,
+                                batch_size=args.per_device_eval_batch_size)
+  model=model.to(device)
+  model.eval()
+  correct_predictions = 0
+  total_predictions = 0
+  scores = 0
+  print('####평가 시작####')
+  for step, batch in enumerate(eval_dataloader):
+      batch = to_device(batch, device)
+      with torch.no_grad():
+          outputs = model(**batch)
+
+      chosen = outputs["chosen_mean_scores"]
+      rejected = outputs["rejected_mean_scores"]
+      correct_predictions += (chosen > rejected).sum()
+      total_predictions += chosen.shape[0]
+      scores += outputs["chosen_mean_scores"].mean().float()
+      if step == 99:  # For faster evaluation and debugging
+          break
+  acc = correct_predictions / total_predictions
+  scores = scores / (step + 1)
+  try:
+      acc = get_all_reduce_mean(acc).item()
+      scores = get_all_reduce_mean(scores).item()
+  except:
+      pass
+  return scores, acc
+
+if __name__ == "__main__":
+    #run_pair_comparison()
+    # run_single_sample()
+    reward_score, acc = evaluation_reward()
+    print_rank_0(
+        f"chosen_last_scores (higher is better) : {reward_score}, acc (higher is better) : {acc}")
