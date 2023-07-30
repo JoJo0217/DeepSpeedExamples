89a15648b16c62dc15e7367538b796ad195488da
pyross <100517226+JoJo0217@users.noreply.github.com>
2023-07-30 18:18:28 +0900

    Create test.sh

---
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/test.sh
@@ -0,0 +1,7 @@
+deepspeed  test.py \
+    --model_name_or_path /content/drive/MyDrive/rlhf/DeepSpeedExamples/applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/output_fullfine0.7 \
+    --num_padding_at_beginning 0 \
+    --per_device_eval_batch_size 2 \
+    --max_seq_len 1024 \
+    --data_path local/jsonfile \
+    --data_split 0,10,0 \
